b743c1a6cbb3b7d9a1b934d012c941c7d6703ab2
Bryan Khor <bryankhor07@gmail.com>
2023-08-30 09:30:05 -0700

    Used 'npm i --save-dev dotenv' to install dotenv

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
@@ -21,7 +25,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', error => console.log(error))
-db.connect('open', () => console.log('Connected to Mongoose'))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 
